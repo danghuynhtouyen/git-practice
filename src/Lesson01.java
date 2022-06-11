76815d9ca1a1460519d106c699b0e63c0088c6de
danghuynhtouyen <danghuynhtouyen@gmail.com>
2022-06-11 22:02:25 +0700

    Bai tap kiem tra so 5

--- src/Lesson01.java
+++ src/Lesson01.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Lesson01 {
 	public static void main(String[] args) {
-	System.out.println("hi welcome");
-}
+		Scanner lp = new Scanner(System.in);
+		while(true) {
+		int number = lp.nextInt() ;
+		if(hamKiemTra(number) == true){
+			System.out.println(number + " is number 5");
+		} else {
+			System.out.println(number + " is not number 5");
+		}
+			
+		}
+		
+	}
+	
+	public static boolean hamKiemTra(int aNumber) {
+		if(aNumber == 5) {
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
 }
